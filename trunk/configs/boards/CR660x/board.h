9dc4f4e92336d71c7c14595de0c4f52ca66fb008
raye85818 <157929770+raye85818@users.noreply.github.com>
2024-02-20 09:59:51 +0800

    Update board.h

--- trunk/configs/boards/CR660x/board.h
+++ trunk/configs/boards/CR660x/board.h
@@ -7,12 +7,12 @@
 #define BOARD_MODEL_URL		"http://www.mi.com/"
 #define BOARD_BOOT_TIME		20
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	18
-#define  BOARD_GPIO_BTN_WPS		7
+#define BOARD_GPIO_BTN_RESET	4
+#define  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
-#undef  BOARD_GPIO_LED_WIFI
-#define  BOARD_GPIO_LED_WAN		15		/* net_blue: 15, net_yellow: 13 */
-#define BOARD_GPIO_LED_POWER	16		/* sys_blue: 16, sys_yellow: 14 */
+#define  BOARD_GPIO_LED_WIFI   8
+#define  BOARD_GPIO_LED_WAN		7		/* net_blue: 15, net_yellow: 13 */
+#define BOARD_GPIO_LED_POWER
 #undef  BOARD_GPIO_LED_LAN
 #undef  BOARD_GPIO_LED_USB
 #undef  BOARD_GPIO_LED_ROUTER
